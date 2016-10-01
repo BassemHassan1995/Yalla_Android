db4d56bc2362a960a7c917874fb3da1e25736c0e
Almostafa Loulah <bassem.hassan1995@gmail.com>
2016-10-01 17:19:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/basse/elregaalateam/BiCepsActivity.java
@@ -0,0 +1,13 @@
+package com.example.basse.elregaalateam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BiCepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bi_ceps);
+    }
+}
